f22f095a3ecd1645d44d8b511c9410b2b76a9ecd
minsu8252 <82499439+minsu8252@users.noreply.github.com>
2021-06-30 20:54:28 +0900

    Add files via upload

--- 기본 문제/03주차_정렬/2750_수 정렬하기/minsu_2750.py
+++ 기본 문제/03주차_정렬/2750_수 정렬하기/minsu_2750.py
@@ -3,13 +3,21 @@ array=[]
 for i in range(n):
     array.append(int(input()))                               # 입력 값으로 배열만들기
 
-
+# 선택 정렬
 for i in range(len(array)): 
     min_index = i                                            # 가장 작은 값 인덱스
     for j in range(i+1, len(array)):                         # for문을 돌리면서 가장 작은 값의 인덱스를 찾기
         if array[min_index] > array[j]:
             min_index = j
     array[i], array[min_index] = array[min_index], array[i]  # 실행하는 자리 인덱스와 가장 작은 값의 인덱스를 바꿈
+
+# 삽입 정렬
+for i in range(1, len(array)):                               # 두번째 값부터 앞의 값들과 비교하기 때문에 range(1, len(array))로 설정
+    for j in range(i, 0, -1):                                # range의 세번째 argument를 -1로 하여 역순으로 찾게함
+        if array[j] < array[j-1]:                            
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
 
 for k in array:
     print(k)

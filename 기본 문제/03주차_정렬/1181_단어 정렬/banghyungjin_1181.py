5f3af946f39d2a91bae87b7f25b601dae8d9bf58
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-01 16:34:54 +0900

    Update banghyungjin_1181.py

--- 기본 문제/03주차_정렬/1181_단어 정렬/banghyungjin_1181.py
+++ 기본 문제/03주차_정렬/1181_단어 정렬/banghyungjin_1181.py
@@ -1,14 +1,13 @@
 import sys
 
-length_of_list = int(sys.stdin.readline().split()[0])       # 리스트 길이 읽어옴
-strings = []
+length_of_list = int(sys.stdin.readline().split()[0])   # 읽어올 리스트의 길이
+strings = []                                            # 리스트
 
-for i in range(length_of_list):                             # 리스트에 단어 추가
-    strings.append(sys.stdin.readline().split()[0])
+for i in range(length_of_list):                         # 리스트 길이 만큼
+    strings.append(sys.stdin.readline().split()[0])     # 내용을 읽어 리스트에 저장
+    
+strings = list(set(strings))                            # 중복값 제거
+strings.sort(key=lambda x: (len(x), x))                 # 정렬 (1번째는 글자 순, 2번째는 사전 순)
 
-strings_set = set(strings)                                  # 증복값 삭제
-strings = list(strings_set)
-strings.sort(key=lambda x: (len(x), x))                     # 정렬 (1번째는 길이순, 2번째는 사전순)
-
-for i in strings:                                           # 정답 출력
+for i in strings:                                       # 정답 출력
     print(i)

16fc39cab1d4066e5d5351e08d083e12981672c1
KANG SEUNG HOON <73418225+Ubinyou@users.noreply.github.com>
2021-07-01 10:01:05 +0900

    Update 강승훈.py

--- 기본 문제/03주차_정렬/1181_단어 정렬/강승훈.py
+++ 기본 문제/03주차_정렬/1181_단어 정렬/강승훈.py
@@ -6,7 +6,7 @@ dict = {} # 키와 밸류로 저장가능한 딕셔너리 일단 생성.
 for _ in range(n): # n 횟수 만큼 입력을 받는데,
     word = stdin.readline().strip() # 이건 그냥 단어
     key = str(len(word)) # 이건 받은 단어에 대해서 길이를 잰다음, 그걸 다시 string 값으로 바꿔서 키로 활용할거임. 예) "1", "2", "7" 등등
-    if key in dict: # 위에서 입력받은 키(단어길이) 값이 딕셔너리에 존재한다면, set 값으로 add 해줌. 예) {"key":("word2","word3","word1")}
+    if key in dict: # 위에서 입력받은 키(단어길이) 값이 딕셔너리에 존재한다면, 이미들어있는 set값에 word add 해줌. 예) {"key":("word2","word3","word1")}
         dict[key].add(word)
     else: # 만약 키 값이 없으면, 그러니까 처음들어오는 단어길이라면, 해당 키값에 대해서, 딕셔너리에 set 타입으로, 단어 하나 넣어줌. 예) {"key":("word1")}
         dict[key] = {word}

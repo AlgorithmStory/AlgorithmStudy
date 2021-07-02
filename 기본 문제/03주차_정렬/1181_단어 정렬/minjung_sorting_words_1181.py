1d5ab654899e3590e6cfa37df6fa9cedb5f42e4c
minjung7961 <42842863+minjung7961@users.noreply.github.com>
2021-07-02 20:28:15 +0900

    Update minjung_sorting_words_1181.py

--- 기본 문제/03주차_정렬/1181_단어 정렬/minjung_sorting_words_1181.py
+++ 기본 문제/03주차_정렬/1181_단어 정렬/minjung_sorting_words_1181.py
@@ -1,30 +1,24 @@
-import sys
-N = int(sys.stdin.readline())                       # 크기를 입력받음
-words = []
+from sys import stdin
+
+N = int(stdin.readline())                       # 크기를 입력받음
+w_dict = {}
 
 for _ in range(N):
-    input = sys.stdin.readline()[:-1]
     
+    word = stdin.readline().strip()
+    key = len(word)
+    if len(word) not in w_dict:                              
+        w_dict[key] = {word}
+    else:
+        w_dict[key].add(word)
 
-    if input in words:                              # 중복된 단어는 추가하지 않습니다
-        continue
-    
-    words.append(input)
 
-words.sort()
+keys = sorted(w_dict)
 
-c_dict = {}                                         # 각요소의 길이 카운팅 계수 저장할 딕셔너리 선언
+for key in keys:
 
-for word in words:
+    words = list(w_dict[key])
+    words.sort()
 
-    if len(word) not in c_dict:                     # 들어온적없는 길이는 새로 선언 
-        c_dict[len(word)] = 0
-            
-    c_dict[len(word)] += 1                           # 각 해당하는 길이에 해당하는 value 1증가시킴
-    
-keys = sorted(c_dict)                                # 키 값에 대하여 오름차순으로 정렬시켜줌
-
-for key in keys:                                        
     for word in words:
-        if key == len(word):
-            print(word)                              # 출력부분
+        print(word)

2a5140599812cd7f245082ba705c3d9ae7317712
minjung7961 <42842863+minjung7961@users.noreply.github.com>
2021-07-02 22:58:12 +0900

    Update and rename minjung_2108_statistics.py.py to minjung_2108_statistics.py

--- 기본 문제/03주차_정렬/2108_통계학/minjung_2108_statistics.py
+++ 기본 문제/03주차_정렬/2108_통계학/minjung_2108_statistics.py
@@ -5,16 +5,19 @@ from sys import stdin
 def mean(i_list):
     return round(sum(i_list)/len(i_list))
 
+
 # 중앙값
 def median(i_list):
     m = int(len(i_list)/2)
     return i_list[m]
+
 
 # 최빈값
 def mode(i_list):
     c_mode = {} # key : 숫자, value: 빈도수 쌍으로 저장할 딕셔너리 선언
     m_mode = 0 # 최대 빈도수 저장할 변수 선언
     n_mode = [] # 최빈값 저장할 변수 선언(2번쨰 큰수 가져올떄 핸들링하기위해 선언)
+
 
     # c_mode 저장
     for n in i_list:
@@ -27,11 +30,13 @@ def mode(i_list):
         if c_mode[n] > m_mode:                                  # 빈도수가 저장된 최대 빈도수보다 크다면
             m_mode = c_mode[n]                                  # 최대 빈도수 갱신
 
-    # 최빈값 저장
+
+    # 최빈값 n_mode에 저장
     for n, b in c_mode.items():
 
         if m_mode == b:
             n_mode.append(n)
+
 
     # 최빈값 여러개면 2번째 큰수 출력
     if len(n_mode) > 1:
@@ -55,8 +60,10 @@ for i in range(N):
     i_list.append(int(stdin.readline()))
 
 
-# ********************* 결과 출력하는 부분 *********************
 i_list.sort()
+
+
+# ********************* 결과 출력하는 부분 *********************
 print(mean(i_list))
 print(median(i_list))
 print(mode(i_list))

2bea7d50c70c1419271f41fec737449b052dba5d
minjung7961 <42842863+minjung7961@users.noreply.github.com>
2021-07-02 10:28:25 +0900

    Update minjung_2751(time_out).py

--- 기본 문제/03주차_정렬/2751_수 정렬하기 2/minjung_2751(time_out).py
+++ 기본 문제/03주차_정렬/2751_수 정렬하기 2/minjung_2751(time_out).py
@@ -2,70 +2,71 @@ import sys
 # ****************필요한변수설정******************
 N = int(sys.stdin.readline())
 input_list = []
+
 for i in range(N):
     input_list.append(int(sys.stdin.readline()))
 
-sorted_list = [-1] * N
 # ******** merge sorting(오름차순) 함수 선언 *****
+def merge(left,right):
     
-def merge(temp_list, left, mid, right):
-    global sorted_list
-#    global deep, sorted_list
-#     print('\t'*deep,'merge!! deep : ',deep,'list : ',temp_list,'left:',left,'mid',mid,'right:',right)
-    i = left
-    j = mid+1
-    k = left
+    sN = len(left)+len(right)
+    s_list = [-1]*sN # 정렬된후의 리스트
+
     # 분할 정렬된 list의 합병
-    while (i<=mid) and (j<=right):
-        if (temp_list[i] <= temp_list[j]):
-            sorted_list[k] = temp_list[i]
-            k += 1
-            i += 1
-        else:
-            sorted_list[k] = temp_list[j]
-            k += 1
-            j += 1
-            
-#     print('\t'*deep,'after while sorted_list : ',sorted_list)
-    # 남아 있는 값들을 일괄 복사 (오른쪽 리스트)
-    if i > mid:
-        for l in range(j,right+1):
-            sorted_list[k] = temp_list[l]
-            k += 1
-    # 남아 있는 값들을 일괄 복사 (왼쪽 리스트)
-    else:
-        for l in range(i,mid+1):
-            sorted_list[k] = temp_list[l]
-            k += 1
-#     print('\t'*deep,'after while sorted_list : ',sorted_list)        
-    # 배열 sorted_list(임시 정렬)의 리스트를 배열 list로 재복사
-    for l in range(left,right+1):
-        temp_list[l] = sorted_list[l]
     
-#     print('\t'*deep,'after merge : ',temp_list)
-    return temp_list
+    s = 0 # s_list의 인덱스
+    r = 0 # right_list 의 인덱스
+    l = 0 # letf_list 의 인덱스
+    
+    
+    while (l < len(left)) and (r < len(right)):
+        
+        if (left[l] <= right[r]):
+            
+            s_list[s] = left[l]
+            l += 1
+            s += 1
+            
+        else:
+            
+            s_list[s] = right[r]
+            r += 1
+            s += 1
+    
+    # 남아 있는 값들을 일괄 복사
 
+    while l < len(left):
+
+        s_list[s] = left[l]
+        s += 1
+        l += 1
+    
+    while r < len(right):
+
+        s_list[s] = right[r]
+        s += 1
+        r += 1
+
+    return s_list
 # ********************** 나누는 함수 선언 **********************
-def divise_list(temp_list,left,right):
-    global deep
-#    deep += 1
-#     print('\t'*deep,'deep : ',deep)
-#     print('\t'*deep,'temp_list :',temp_list,'left :',left,'right :',right)
-    # basement 유무 검사 
-    if left<right :
-        mid = int((left + right)/2)
-        divise_list(temp_list, left, mid)
-        divise_list(temp_list, mid+1, right)
-        temp_list = merge(temp_list, left, mid, right)
-#    else:
-#         print('\t'*deep,'basement')
-              
-#    deep =  deep - 1
-    return temp_list
+def d_list(input_list):
 
+    # basement 유무 검사 
+    if len(input_list) > 1 :        
+        mid = int(len(input_list)/2)
+        left = input_list[:mid]
+        right = input_list[mid:]
+       
+        
+        left = d_list(left)
+        right = d_list(right)
+        input_list = merge(left,right)
+
+    return input_list
+            
 # ************* 함수실행 및 출력******************
 
-output_list = divise_list(input_list,0,N-1)
+output_list = d_list(input_list)
 
 for i in output_list:
     print(i)

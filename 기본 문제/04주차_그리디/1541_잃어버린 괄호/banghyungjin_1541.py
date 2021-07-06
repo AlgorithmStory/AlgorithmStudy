30a8b0b3b063508a531924450af0be9492d7701f
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-06 10:54:53 +0900

    Update banghyungjin_1541.py

--- 기본 문제/04주차_그리디/1541_잃어버린 괄호/banghyungjin_1541.py
+++ 기본 문제/04주차_그리디/1541_잃어버린 괄호/banghyungjin_1541.py
@@ -5,8 +5,8 @@ is_positive = 1                                                                 
 input_number = ''                                                                           # 넣을 숫자
 input_numbers = []                                                                          # 숫자 리스트
 for i in func:                                                                              # 식을 읽어와서 부호 기준으로 나눔
-    if i == '-':
-        if int(input_number) > 0 and len(input_numbers) > 0 and input_numbers[-1] < 0:
+    if i == '-':                                                                            # 숫자가 아닌 문자를 읽을 때마다 앞의 부호와 지금 숫자를 더해서 리스트에 저장
+        if int(input_number) > 0 and len(input_numbers) > 0 and input_numbers[-1] < 0:      # - 다음 + 숫자가 올 경우 리스트에 그냥 추가하지 않고 - 에 다시 빼줌
             input_numbers[-1] -= int(input_number)
         else:
             input_numbers.append(is_positive * int(input_number))

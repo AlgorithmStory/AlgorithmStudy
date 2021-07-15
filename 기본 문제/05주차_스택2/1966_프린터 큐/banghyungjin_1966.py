c0b60cb1f4489efcd0432b0c370a971a6c6ddfb5
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-15 13:57:24 +0900

    Update banghyungjin_1966.py

--- 기본 문제/05주차_스택2/1966_프린터 큐/banghyungjin_1966.py
+++ 기본 문제/05주차_스택2/1966_프린터 큐/banghyungjin_1966.py
@@ -11,14 +11,12 @@ for i in range(numbers_of_tests):                                       # 테스
     for j in range(test[0]):                                            # N 만큼
         test_cases.append([j, importance[j]])                           # 문서 리스트에 문서 추가
 
-    count = 1                                                           # 인쇄된 문서 숫자
     # 현재 문서 리스트의 첫번째 프린트가 원하는 프린트이면서 그걸 인쇄해야하는 상황이 아닐때
-    while test_cases[0] != wanted_print or wanted_print[1] != max(test_cases, key=lambda x:x[1])[1]:
-        if test_cases[0][1] == max(test_cases, key=lambda x:x[1])[1]:   # 만약 리스트의 첫 프린트를 인쇄해야하면
+    while test_cases[0] != wanted_print or wanted_print[1] != max(test_cases, key=lambda x: x[1])[1]:
+        if test_cases[0][1] == max(test_cases, key=lambda x: x[1])[1]:  # 만약 리스트의 첫 프린트를 인쇄해야하면
             test_cases.pop(0)                                           # 리스트에서 그냥 제거
-            count += 1                                                  # 인쇄된 문서 숫자 추가
         else:                                                           # 만약 리스트의 첫 프린트를 인쇄하는게 아니면
             test_cases.append(test_cases[0])                            # 첫 프린트를 마지막으로 이동
             test_cases.pop(0)                                           # -
-
-    print(count)                                                        # while 문을 빠져나오면 원하는 문서가 출력된다는 의미이므로 카운트 출력
+    # while 문을 빠져나오면 원하는 문서가 출력된다는 의미 
+    print(test[0] - len(test_cases) + 1)                                # N - 현재 남은 프린트 개수 + 1 출력                     

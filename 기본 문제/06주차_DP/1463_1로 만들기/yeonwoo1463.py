a2663eda9275f222f8f8b62f818328293ae566cb
yeonwoo780 <82499591+yeonwoo780@users.noreply.github.com>
2021-07-19 17:40:36 +0900

    Add files via upload

--- 기본 문제/06주차_DP/1463_1로 만들기/yeonwoo1463.py
+++ 기본 문제/06주차_DP/1463_1로 만들기/yeonwoo1463.py
@@ -10,17 +10,17 @@ while True:
  
     if min_count_index <= 1: # 0, 1까지는 연산횟수가  0이므로 0으로 설정
         min_count_1[min_count_index] = 0
+
     else: # 미리 계산한 인덱스 값에 대한 최댓값 구하는 방식
         result = X + 1
-        if min_count_index % 3 == 0:
-            min_index = min_count_index // 3
-            result = min(result, min_count_1[min_index])
-            
+        if min_count_index % 3 == 0: 
+            result = min(result, min_count_1[min_count_index // 3])
+
         if min_count_index % 2 == 0:
-            min_index = min_count_index // 2
-            result = min(result, min_count_1[min_index])
+            result = min(result, min_count_1[min_count_index // 2])
  
         result = min(result, min_count_1[min_count_index-1])
+
         min_count_1[min_count_index] = int(result + 1)
     min_count_index = min_count_index + 1
     

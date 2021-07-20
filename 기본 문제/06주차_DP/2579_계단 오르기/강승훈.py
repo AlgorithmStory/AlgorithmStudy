9f5fde5096f2a62099c375c15db7f18fc09fb17e
KANG SEUNG HOON <73418225+Ubinyou@users.noreply.github.com>
2021-07-20 19:03:00 +0900

    Create 강승훈.py

---
+++ 기본 문제/06주차_DP/2579_계단 오르기/강승훈.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+# 입력
+n = int(stdin.readline())
+stairs = [int(stdin.readline()) for _ in range(n)]
+
+# 메모이제이션 변수.
+memo = [-1 for _ in range(n)]
+
+# 재귀 함수 (처음 실행시 마지막 위치 값을 넣어줌).
+def recursion(index):
+    # 첫번째 위치에선, 첫번째 계단이라는 경우밖에 없으니 첫번째 계단 값 리턴.
+    if index == 0:
+        return stairs[0]
+
+    # 두번째 경우에선, 첫번쨰와 두번째 값을 합한 값이 가장 큰 경우이므로 두개를 더해서 리턴.
+    if index == 1:
+        return stairs[0] + stairs[1]
+
+    # 인덱스가 위치를 벗어나면 걍 리턴.
+    if index < 0:
+        return 0
+
+    # 이미 계산 했던 값이면, 계산 했던 값 리턴.
+    if memo[index] != -1:
+        return memo[index]
+
+    # n번째 계단은 n-3과 n-1을 거쳐서 오는 경우와, 과 n-2 번째 계단에서 올라오는 두가지 경우만 존재함.
+    memo[index] = max(recursion(index - 3) + stairs[index - 1], recursion(index - 2)) + stairs[index]
+
+    # 둘중 큰 값에 현재의 계단 값을 더해서 취하면 됨.
+    return memo[index]
+
+# 출력.
+print(recursion(n - 1))

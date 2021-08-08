8b475433b8f76710e44aa0827de40fa430b32af1
KANG SEUNG HOON <73418225+Ubinyou@users.noreply.github.com>
2021-08-08 16:23:52 +0900

    Update 강승훈.py

--- 기본 문제/09주차_이분 탐색/1920_수 찾기/강승훈.py
+++ 기본 문제/09주차_이분 탐색/1920_수 찾기/강승훈.py
@@ -1,7 +1,6 @@
 # 입력.
 n = int(input())
-n_arr = list(map(int, input().split(" ")))
-n_set = set(n_arr)
+n_set = set(map(int, input().split(" ")))
 m = int(input())
 m_arr = list(map(int, input().split(" ")))
 

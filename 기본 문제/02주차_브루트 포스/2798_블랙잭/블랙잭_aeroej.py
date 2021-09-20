c55c63c1922f47972dcb9d2946d4949d6b8ea55e
aeroej <asd0755@naver.com>
2021-09-21 01:08:02 +0900

    Modify 블랙잭_aeroej.py 주석 추가

--- 기본 문제/02주차_브루트 포스/2798_블랙잭/블랙잭_aeroej.py
+++ 기본 문제/02주차_브루트 포스/2798_블랙잭/블랙잭_aeroej.py
@@ -8,24 +8,24 @@ def isMax(num):
 
 
 def dfs(v, cnt):
-  if cnt >= 3:
+  if cnt >= 3:  # 플레이어가 3장을 고른 경우
     isMax(sum(lst))
     return
-  if v >= len(cards):
+  if v >= len(cards):  # 모든 카드를 확인한 경우
     return
 
   lst.append(cards[v])
-  dfs(v+1, cnt+1)
+  dfs(v+1, cnt+1)  # v번째 카드를 고른 경우
   lst.pop()
-  dfs(v+1, cnt)
+  dfs(v+1, cnt)  # v번째 카드를 고르지 않은 경우
 
 
 if __name__ == "__main__":
   n, m = map(int, sys.stdin.readline().rstrip().split())
   cards = list(map(int, sys.stdin.readline().rstrip().split()))
 
-  lst = []
-  maX = 0
+  lst = []  # 플레이어가 고른 카드
+  maX = 0  # M을 넘지 않으면서 M과 최대한 가까운 카드의 합
 
   dfs(0, 0)
   print(maX)
